1782abbc6f9a41e6e4c6fea642d36d47a14f8ad6
hari <hariprasadai@anurag.edu.in>
2023-08-08 10:14:44 +0530

    first commit

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
 const ctrlMain = require('../controllers/main');
+const ctrlLocations = require('../controllers/locations');
+const ctrlOthers = require('../controllers/others');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  //res.render('index', { title: 'Express' });
-  res.render('index', { title: 'My project' });
-});
-/* GET homepage. */
-router.get('/', ctrlMain.index);
-
+/* Locations pages */
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+/* Other pages */
+router.get('/about', ctrlOthers.about);
 module.exports = router;
